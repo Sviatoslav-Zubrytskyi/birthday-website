0c3c0e328ed3e9ece2bf1024dfe487111017e351
Sviatoslav-Zubrytskyi <sviatoslav.zubrytskyi@student.nhlstenden.com>
2023-11-27 06:40:30 +0100

    added bg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
-
+import confetti from 'https://cdn.skypack.dev/canvas-confetti';
+import balloon from './baloon.png'
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const makeConfetti = confetti();
+    window.onload = makeConfetti;
+    return (
+        <div className="cantainer">
+            <div className="balloons">
+                <span style={{ "--i": 15 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 20 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 23 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 9 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 13 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 11 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 17 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 19 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+                <span style={{ "--i": 30 }}><img className="bg-balloon" src={balloon} alt="balloon"/></span>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
